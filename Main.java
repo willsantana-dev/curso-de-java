3c13efc8cf054c7cda80e2552402b3ba9c67d877
willsantana-dev <willsantana.dev@gmail.com>
2026-01-15 20:29:32 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double a = sc.nextDouble();
+        System.out.print("Digite o segundo número: ");
+        double b = sc.nextDouble();
+        System.out.print("Digite o terceiro número: ");
+        double c = sc.nextDouble();
+
+        double triangulo = a * c / 2;
+        double circulo = Math.PI * Math.pow(c, 2);
+        double trapezio = (a + b) * c / 2;
+        double quadrado = b * b;
+        double retangulo = a * b;
+
+        System.out.printf("%nTriângulo: %.3f", triangulo);
+        System.out.printf("%nCírculo: %.3f", circulo);
+        System.out.printf("%nTrapézio: %.3f", trapezio);
+        System.out.printf("%nQuadrado: %.3f", quadrado);
+        System.out.printf("%nRetângulo: %.3f", retangulo);
+
+        sc.close();
+    }
+}
